32995a78bb89a80af08328bf26f3979bc4855968
杨益 <yangyi.cn.gz@gmail.com>
2024-05-06 12:21:28 +0800

    feat: 检查两个返回值的must return

--- must.go
+++ must.go
@@ -23,3 +23,21 @@ func MustReturn[T any](value T, err error) T {
 	}
 	return value
 }
+
+// MustReturn2 用于检查两个有效返回值时的错误，如果错误不为空，则抛出异常
+//
+// Example:
+//
+//	func Foo() (int, string, error) {
+//		// ...
+//	}
+//
+//	var n int
+//	var s string
+//	n, s = MustReturn2(Foo())
+func MustReturn2[T1 any, T2 any](t1 T1, t2 T2, err error) (T1, T2) {
+	if err != nil {
+		panic(err)
+	}
+	return t1, t2
+}
