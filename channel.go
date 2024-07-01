f4669959cf4dbc921676341bb07031cb4f1f7304
杨益 <yangyi.cq@qq.com>
2024-07-01 13:36:32 +0800

    refactor: 代码改进

--- channel.go
+++ channel.go
@@ -3,7 +3,7 @@ package gokit
 import "sync"
 
 // FanOut is a function that will fan out the input channel to multiple workers
-func FanOut[T any](input chan T, workerNum int, worker func(v T)) {
+func FanOut[T any](input <-chan T, workerNum int, worker func(v T)) {
 	for i := 0; i < workerNum; i++ {
 		go func() {
 			for v := range input {
@@ -14,7 +14,7 @@ func FanOut[T any](input chan T, workerNum int, worker func(v T)) {
 }
 
 // FanIn is a function that will fan in multiple input channels to a single output channel
-func FanIn[T any](input ...chan T) chan T {
+func FanIn[T any](input ...<-chan T) <-chan T {
 	output := make(chan T)
 
 	go func() {
@@ -23,7 +23,7 @@ func FanIn[T any](input ...chan T) chan T {
 
 		for _, v := range input {
 			wg.Add(1)
-			go func(c chan T) {
+			go func(c <-chan T) {
 				defer wg.Done()
 
 				for v := range c {
