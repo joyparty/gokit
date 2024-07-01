a59edde9ed5c2cc20029610c1683972c55a2b771
杨益 <yangyi.cq@qq.com>
2024-07-01 15:19:40 +0800

    refactor: FanOut增加返回值，标识所有worker已结束退出

--- channel.go
+++ channel.go
@@ -1,16 +1,31 @@
 package gokit
 
-import "sync"
+import (
+	"sync"
+)
 
 // FanOut is a function that will fan out the input channel to multiple workers
-func FanOut[T any](input <-chan T, workerNum int, worker func(v T)) {
-	for i := 0; i < workerNum; i++ {
-		go func() {
-			for v := range input {
-				worker(v)
-			}
-		}()
-	}
+func FanOut[T any](input <-chan T, workerNum int, worker func(v T)) (done <-chan struct{}) {
+	doneC := make(chan struct{})
+
+	go func() {
+		defer close(doneC)
+
+		var wg sync.WaitGroup
+		for i := 0; i < workerNum; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				for v := range input {
+					worker(v)
+				}
+			}()
+		}
+		wg.Wait()
+	}()
+
+	return doneC
 }
 
 // FanIn is a function that will fan in multiple input channels to a single output channel
