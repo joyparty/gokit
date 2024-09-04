dc28661198c7a947a7a52edcdd12744953c9e554
yangyi <yangyi.cq@qq.com>
2024-09-04 20:56:11 +0800

    refactor: rename MustTo() to MustBe()

--- must.go
+++ must.go
@@ -47,11 +47,18 @@ func MustReturn2[T1 any, T2 any](t1 T1, t2 T2, err error) (T1, T2) {
 }
 
 // MustTo 类型转换失败则panic
+//
+// Deprecated: Use MustBe() instead.
 func MustTo[T any](v any) T {
+	return MustBe[T](v)
+}
+
+// MustBe 类型断言，失败则panic
+func MustBe[T any](v any) T {
 	if x, ok := v.(T); ok {
 		return x
 	}
 
 	var x T
-	panic(fmt.Errorf("%T cannot convert to %T", v, x))
+	panic(fmt.Errorf("%T is not %T", v, x))
 }
